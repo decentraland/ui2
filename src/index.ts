e24487d9fd932e3beac1e7100eb52a38ce5654bb
Braian Mellor <braianj@gmail.com>
2023-11-03 14:19:41 -0300

    fix: downgrade react version. expose emotion (#4)

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "@mui/material"
+
+import emotionCache from "@emotion/cache"
+import emotionReact from "@emotion/react"
+import emotionStyled from "@emotion/styled"
+export { emotionReact, emotionStyled, emotionCache }
