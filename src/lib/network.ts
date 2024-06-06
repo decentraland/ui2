3f38201af8e06896fa15d6931caecf7ca8454aca
Braian Mellor <braianj@gmail.com>
2024-06-06 16:36:48 -0300

    feat: change gitignore

---
+++ src/lib/network.ts
@@ -0,0 +1,25 @@
+import { Network } from "@dcl/schemas"
+
+export type NetworksNames = {
+  [key in Network]: string
+}
+
+export const networksNames: NetworksNames = {
+  [Network.ETHEREUM]: "Ethereum",
+  [Network.MATIC]: "Polygon",
+  [Network.ARBITRUM]: "Arbitrum",
+  [Network.AVALANCHE]: "Avalanche",
+  [Network.FANTOM]: "Fantom",
+  [Network.BSC]: "Binance Smart Chain",
+  [Network.OPTIMISM]: "Optimism",
+}
+
+export const getNetworkName = (network: Network): string | undefined =>
+  networksNames[network]
+
+export const getNetworkMANADescription = (
+  network: Network
+): string | undefined => {
+  const networkName = getNetworkName(network)
+  return networkName ? `${networkName} MANA` : "MANA"
+}
