066a8f83a770754fdaa91efbe6ae7052d39180c7
Braian Mellor <braianj@gmail.com>
2024-06-19 14:06:05 -0300

    fix: prettier and test

--- src/components/AddressField/utils.ts
+++ src/components/AddressField/utils.ts
@@ -1,7 +1,7 @@
-export function isValid(addr: string) {
+export function isValid(addr = "") {
   return /^0x[a-fA-F0-9]{40}$/g.test(addr)
 }
 
-export function shorten(address: string) {
-  return address ? address.slice(0, 6) + '...' + address.slice(42 - 5) : ''
+export function shorten(address?: string) {
+  return address ? address.slice(0, 6) + "..." + address.slice(42 - 5) : ""
 }
