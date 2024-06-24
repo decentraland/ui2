849b5e817c6256a2f7d3c07dee8604f5d16a37e9
Braian Mellor <braianj@gmail.com>
2024-06-24 10:50:29 -0300

    feat: Mana component

---
+++ src/components/Mana/Mana.types.ts
@@ -0,0 +1,17 @@
+import { Network } from "@dcl/schemas"
+
+export type ManaProps = {
+  size?: "small" | "medium" | "large"
+  inline?: boolean
+  network?: Network
+  children?: React.ReactChild
+  color?: string
+  primary?: boolean
+  showTooltip?: boolean
+  href?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement> &
+    ((
+      event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+      network: Network
+    ) => void)
+}
