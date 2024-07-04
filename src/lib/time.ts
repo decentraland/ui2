032237646c5d82d7a13127972f9ef417ffb2ca55
Braian Mellor <braianj@gmail.com>
2024-07-04 09:56:56 -0300

    feat: remove lib time

--- src/lib/time.ts
+++
@@ -1,9 +0,0 @@
-import Time from "dayjs"
-import relativeTime from "dayjs/plugin/relativeTime"
-import "dayjs/locale/zh"
-import "dayjs/locale/es"
-import "dayjs/locale/en"
-
-Time.extend(relativeTime)
-
-export { Time }
