1c0b4bd806f8f4843baef79e1bda4db5b4a878d5
Braian Mellor <braianj@gmail.com>
2024-07-05 12:16:50 -0300

    feat: UserMenuSignedin component and ManaBalances component

---
+++ src/components/UserMenu/ManaBalances/ManaBalances.stories.tsx
@@ -0,0 +1,34 @@
+import { Network } from "@dcl/schemas"
+import { ManaBalances } from "./ManaBalances"
+import { ManaBalancesProps } from "./ManaBalances.types"
+import type { Meta, StoryObj } from "@storybook/react"
+
+const balance = {
+  [Network.ETHEREUM]: 8888.88,
+  [Network.MATIC]: 8888.88,
+}
+
+const meta: Meta<ManaBalancesProps> = {
+  component: ManaBalances,
+  title: "Decentraland UI/ManaBalances",
+  tags: ["autodocs"],
+  argTypes: {
+    manaBalances: {
+      description: "Balance of MANA in different networks",
+      control: {
+        type: "object",
+      },
+      options: [balance],
+    },
+  },
+  render: (args) => <ManaBalances manaBalances={balance} {...args} />,
+}
+
+type Story = StoryObj<ManaBalancesProps>
+
+// eslint-disable-next-line import/no-default-export
+export default meta
+
+export const Simple: Story = {
+  args: {},
+}
