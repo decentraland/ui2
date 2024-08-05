19ac810758bf109df5facb93acc2f812c01f40aa
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2024-08-05 16:45:05 +0200

    Update src/components/Blockie/Blockie.styled.ts
    Co-authored-by: Braian Mellor <braianj@gmail.com>
    Signed-off-by: Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>

--- src/components/Blockie/Blockie.styled.ts
+++ src/components/Blockie/Blockie.styled.ts
@@ -4,7 +4,9 @@ import { Box } from "@mui/material"
 const StyledCanvas = styled("canvas")<{
   scale: number
   size: number
-}>(({ scale, size }) => ({
+}>((props) => {
+const { scale, size } = props
+return {
   borderRadius: 6,
   verticalAlign: "text-bottom",
   lineHeight: "0.5em",
@@ -12,7 +14,7 @@ const StyledCanvas = styled("canvas")<{
   ...(scale * size <= 16 && { borderRadius: 1.5 }),
   ...(scale * size <= 24 && { borderRadius: 3 }),
   ...(scale * size <= 36 && { borderRadius: 4 }),
-}))
+}})
 
 const StyledBlockieChildrenWrapper = styled(Box)({
   marginLeft: "0.5em",
