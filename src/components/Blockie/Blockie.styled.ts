9c2a4fb7945bea3fe59b6abf8c922601e4146ecb
Kevin Szuchet <kevin.szuchet@decentraland.org>
2024-08-05 16:57:02 +0200

    chore: apply prettier rules

--- src/components/Blockie/Blockie.styled.ts
+++ src/components/Blockie/Blockie.styled.ts
@@ -5,16 +5,18 @@ const StyledCanvas = styled("canvas")<{
   scale: number
   size: number
 }>((props) => {
-const { scale, size } = props
-return {
-  borderRadius: 6,
-  verticalAlign: "text-bottom",
-  lineHeight: "0.5em",
-  marginLeft: "0.1em",
-  ...(scale * size <= 16 && { borderRadius: 1.5 }),
-  ...(scale * size <= 24 && { borderRadius: 3 }),
-  ...(scale * size <= 36 && { borderRadius: 4 }),
-}})
+  const { scale, size } = props
+
+  return {
+    borderRadius: 6,
+    verticalAlign: "text-bottom",
+    lineHeight: "0.5em",
+    marginLeft: "0.1em",
+    ...(scale * size <= 16 && { borderRadius: 1.5 }),
+    ...(scale * size <= 24 && { borderRadius: 3 }),
+    ...(scale * size <= 36 && { borderRadius: 4 }),
+  }
+})
 
 const StyledBlockieChildrenWrapper = styled(Box)({
   marginLeft: "0.5em",
