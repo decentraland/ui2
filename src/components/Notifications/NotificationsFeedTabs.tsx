9c8e91a4611610e86738a6a89cccda08b308b98a
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2024-09-05 17:13:28 +0100

    fix: Avoid trying to show non mapped notifications (#191)
    * fix: Avoid trying to show non mapped notifications
    
    * chore(deps): bump schemas

--- src/components/Notifications/NotificationsFeedTabs.tsx
+++ src/components/Notifications/NotificationsFeedTabs.tsx
@@ -34,14 +34,17 @@ const NotificationHandler = ({
   renderProfile?: (address: string) => JSX.Element | string | null
 }) => {
   const NotificationComponent = NotificationComponentByType[notification.type]
+
+  if (!NotificationComponent) {
+    return null
+  }
+
   return (
-    NotificationComponent && (
-      <NotificationComponent
-        notification={notification}
-        locale={locale}
-        renderProfile={renderProfile}
-      />
-    )
+    <NotificationComponent
+      notification={notification}
+      locale={locale}
+      renderProfile={renderProfile}
+    />
   )
 }
 
