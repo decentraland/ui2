3098b7847ecccbdbcea0c7f1d03f8020bae1e566
Braian Mellor <braianj@gmail.com>
2024-11-22 12:56:59 -0300

    feat: expose ManaBalances (#204)

--- src/components/UserMenu/index.ts
+++ src/components/UserMenu/index.ts
@@ -1,4 +1,7 @@
 export { UserMenu } from "./UserMenu"
 export { UserMenuSignedIn } from "./UserMenuSignedIn/UserMenuSignedIn"
+export { ManaBalances } from "./ManaBalances"
+
 export type { UserMenuProps } from "./UserMenu.types"
 export type { UserMenuSignedInProps } from "./UserMenuSignedIn/UserMenuSignedIn.types"
+export type { ManaBalancesProps } from "./ManaBalances"
