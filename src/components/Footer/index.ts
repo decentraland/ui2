ec5898483f28bd1790cdea36a961d7a7b73ff0c3
Braian Mellor <braianj@gmail.com>
2025-07-29 16:43:57 -0300

    feat: footer component

---
+++ src/components/Footer/index.ts
@@ -0,0 +1,8 @@
+export { Footer } from "./Footer"
+export type {
+  FooterProps,
+  FooterLink,
+  SocialLink,
+  Language,
+  SupportedLanguage,
+} from "./Footer.types"
---
+++ src/components/Icon/Social/SocialIcons.stories.tsx
@@ -0,0 +1,56 @@
+import { DiscordIcon } from "./DiscordIcon"
+import { GitHubIcon } from "./GitHubIcon"
+import { RedditIcon } from "./RedditIcon"
+import { XIcon } from "./XIcon"
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof DiscordIcon> = {
+  title: "components/Icon/Social",
+  component: DiscordIcon,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+}
+
+type Story = StoryObj<typeof DiscordIcon>
+
+const Discord: Story = {}
+
+const GitHub: Story = {
+  render: () => <GitHubIcon />,
+}
+
+const Reddit: Story = {
+  render: () => <RedditIcon />,
+}
+
+const X: Story = {
+  render: () => <XIcon />,
+}
+
+const AllSocialIcons: Story = {
+  render: () => (
+    <div style={{ display: "flex", gap: "16px", alignItems: "center" }}>
+      <DiscordIcon />
+      <RedditIcon />
+      <GitHubIcon />
+      <XIcon />
+    </div>
+  ),
+}
+
+const LargeSocialIcons: Story = {
+  render: () => (
+    <div style={{ display: "flex", gap: "16px", alignItems: "center" }}>
+      <DiscordIcon fontSize="large" />
+      <RedditIcon fontSize="large" />
+      <GitHubIcon fontSize="large" />
+      <XIcon fontSize="large" />
+    </div>
+  ),
+}
+
+// eslint-disable-next-line import/no-default-export
+export default meta
+export { Discord, GitHub, Reddit, X, AllSocialIcons, LargeSocialIcons }
