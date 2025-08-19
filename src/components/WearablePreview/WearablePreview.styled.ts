37645ec4cea4bd21b89d004090f17b91cecf7fcf
Braian Mellor <braianj@gmail.com>
2025-08-19 15:58:10 -0300

    fix: wearable preview background (#274)

--- src/components/WearablePreview/WearablePreview.styled.ts
+++ src/components/WearablePreview/WearablePreview.styled.ts
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled"
 
-const StyledWearablePreview = styled("iframe")(({ theme }) => ({
-  background: theme.palette.background.default,
+const StyledWearablePreview = styled("iframe")({
+  background: "transparent",
   border: 0,
   width: "100%",
   height: "100%",
-}))
+})
 
 export { StyledWearablePreview }
