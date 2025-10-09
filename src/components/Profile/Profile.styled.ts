3dcc0c99ab8b63b39543395ae4fb956f5dd598d1
Mauro Cicerchia <maurocicerchia98@gmail.com>
2025-10-09 14:41:53 -0300

    fix: variable gap

--- src/components/Profile/Profile.styled.ts
+++ src/components/Profile/Profile.styled.ts
@@ -2,9 +2,9 @@ import styled, { CSSObject } from "@emotion/styled"
 import { ProfileBaseProps } from "./Profile.types"
 
 const ProfileContainer = styled("span")<ProfileBaseProps>(
-  ({ theme, inline = true }) => ({
+  ({ theme, inline = true, size = "normal" }) => ({
     display: inline ? "inline-flex" : "flex",
-    gap: theme.spacing(0.5),
+    gap: size !== "normal" ? theme.spacing(1) : theme.spacing(0.5),
     alignItems: "center",
     verticalAlign: "top",
   })
