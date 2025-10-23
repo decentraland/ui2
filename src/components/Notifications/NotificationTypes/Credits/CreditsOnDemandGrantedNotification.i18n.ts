b25c0e9a8d3dddc943c4dd34650a19e662916c28
LautaroPetaccio <lausuper@gmail.com>
2025-10-23 15:02:31 -0300

    feat: Add Credits On Demand Granted notification

---
+++ src/components/Notifications/NotificationTypes/Credits/CreditsOnDemandGrantedNotification.i18n.ts
@@ -0,0 +1,17 @@
+const i18n = {
+  en: {
+    description:
+      "Congrats! You've earned {amount} extra Credits for this season. Make sure to use them before they expire!",
+    title: "Bonus Credits Unlocked!",
+  },
+  es: {
+    description: "",
+    title: "",
+  },
+  zh: {
+    description: "",
+    title: "",
+  },
+}
+
+export { i18n }
