400c34b7cc9c717e268e8d6b5f987e7bdb1ffc6a
Braian Mellor <braian.mellor@decentraland.org>
2025-10-28 09:32:26 -0300

    feat: update logo story

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,17 +1,40 @@
 import { SvgIconProps } from "@mui/material"
 import { Logo } from "./Logo"
+import { StoryContainer, StoryItemContainer } from "./Logo.styled"
 import type { Meta, StoryObj } from "@storybook/react"
 
 const meta: Meta = {
   component: Logo,
   title: "Decentraland UI/Logo",
+  tags: ["autodocs"],
   render: () => <Logo />,
 }
 
 type Story = StoryObj<SvgIconProps>
 
-const Decentraland: Story = {}
+const All: Story = {
+  render: () => (
+    <StoryContainer>
+      <StoryItemContainer>
+        <h3>Normal</h3>
+        <Logo size="normal" />
+      </StoryItemContainer>
+      <StoryItemContainer>
+        <h3>Large</h3>
+        <Logo size="large" />
+      </StoryItemContainer>
+      <StoryItemContainer>
+        <h3>Huge</h3>
+        <Logo size="huge" />
+      </StoryItemContainer>
+      <StoryItemContainer>
+        <h3>Massive</h3>
+        <Logo size="massive" />
+      </StoryItemContainer>
+    </StoryContainer>
+  ),
+}
 
 // eslint-disable-next-line import/no-default-export
 export default meta
-export { Decentraland }
+export { All }
