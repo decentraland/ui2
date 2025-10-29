d4ddfd1406073d4cd71a3de48d359b2c16de248e
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-10-29 15:11:39 -0300

    fix: Styles

--- src/components/WearablePreview/TranslationControls/TranslationControls.styled.ts
+++ src/components/WearablePreview/TranslationControls/TranslationControls.styled.ts
@@ -1,7 +1,8 @@
 import styled from "@emotion/styled"
+import { Box } from "@mui/material"
 import { VerticalPosition } from "./TranslationControls.types"
 
-const StyledTranslationControlsContainer = styled("div")({
+const StyledTranslationControlsContainer = styled(Box)({
   position: "absolute",
   top: 0,
   left: 0,
@@ -13,30 +14,28 @@ const StyledTranslationControlsContainer = styled("div")({
   },
 })
 
-const StyledVerticalSliderContainer = styled("div")<{
+const StyledVerticalSliderContainer = styled(Box)<{
   verticalPosition: VerticalPosition
 }>(({ verticalPosition }) => ({
   position: "absolute",
   transform: "rotate(-90deg)",
-  top: "181px",
-  width: "280px",
+  top: 176,
+  width: 280,
   display: "flex",
   flexDirection: "row-reverse",
   ...(verticalPosition === VerticalPosition.RIGHT
-    ? { right: "-105px" }
-    : { left: "-105px" }),
+    ? { right: -104 }
+    : { left: -104 }),
 }))
 
-const StyledIconWrapper = styled("div")({
-  marginTop: "-6px",
-  marginLeft: "12px",
+const StyledIconWrapper = styled(Box)({
   display: "flex",
   alignItems: "center",
   color: "#736e7d",
 })
 
-const StyledRangeInput = styled("input")({
-  width: "229px",
+const StyledRangeInput = styled("input")(({ theme }) => ({
+  width: 224,
   background: "none",
   WebkitAppearance: "none",
   "&:focus": {
@@ -44,49 +43,49 @@ const StyledRangeInput = styled("input")({
   },
   "&::-webkit-slider-thumb": {
     WebkitAppearance: "none",
-    width: "32px",
-    height: "8px",
-    borderRadius: "4px",
+    width: 32,
+    height: 8,
+    borderRadius: 4,
     background: "#736e7d",
     boxShadow:
       "0px 2px 4px rgba(0, 0, 0, 0.16), inset 0px 1px 0px rgba(255, 255, 255, 0.16)",
     overflow: "visible",
     cursor: "pointer",
-    paddingBottom: "5px",
-    marginTop: "-3px",
+    paddingBottom: theme.spacing(0.625),
+    marginTop: theme.spacing(-0.375),
   },
   "&::-moz-range-thumb": {
-    width: "24px",
-    height: "24px",
-    borderRadius: "20px",
+    width: 24,
+    height: 24,
+    borderRadius: 20,
     backgroundColor: "#736e7d",
     overflow: "visible",
     cursor: "pointer",
     border: "none",
   },
   "&::-ms-thumb": {
-    width: "24px",
-    height: "24px",
-    borderRadius: "20px",
+    width: 24,
+    height: 24,
+    borderRadius: 20,
     backgroundColor: "#736e7d",
     overflow: "visible",
     cursor: "pointer",
   },
   "&::-webkit-slider-runnable-track": {
-    width: "300px",
-    height: "2px",
+    width: 304,
+    height: 2,
     background: "#000000",
     border: "none",
-    borderRadius: "10px",
+    borderRadius: 10,
   },
   "&::-moz-range-track": {
     width: "100%",
-    height: "15px",
-    borderRadius: "10px",
+    height: 16,
+    borderRadius: 10,
     cursor: "pointer",
     background: "rgba(115, 110, 125, 0.32)",
   },
-})
+}))
 
 export {
   StyledTranslationControlsContainer,
