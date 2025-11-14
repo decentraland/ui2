eff1996b36e8b5878e802b6d7fc17827efe6d78e
Braian Mellor <braianj@gmail.com>
2025-11-14 17:19:30 -0300

    feat: apply standard

--- src/components/FooterLanding/FooterLanding.styled.ts
+++ src/components/FooterLanding/FooterLanding.styled.ts
@@ -1,142 +1,121 @@
-import { Box, Button, Link, Typography, styled, useTheme } from "@mui/material"
+import { Box, Button, Link, Typography, styled } from "@mui/material"
 
-const FooterContainer = styled("footer")(() => {
-  const theme = useTheme()
-  return {
-    display: "flex",
-    justifyContent: "space-between",
-    backgroundColor: theme.palette.background.default,
-    zIndex: 11,
-    height: "475px",
-    position: "relative",
-    paddingTop: "96px",
-    paddingLeft: "160px",
-    paddingRight: "160px",
-    [theme.breakpoints.down("xl")]: {
-      paddingLeft: "80px",
-      paddingRight: "80px",
-    },
-    [theme.breakpoints.down("md")]: {
-      paddingLeft: "54px",
-      paddingRight: "54px",
-    },
-    [theme.breakpoints.down("xs")]: {
-      flexDirection: "column",
-      alignItems: "center",
-      paddingLeft: "8px",
-      paddingRight: "8px",
-      height: "630px",
-    },
-  }
-})
+const FooterContainer = styled("footer")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  backgroundColor: theme.palette.background.default,
+  zIndex: 11,
+  height: "475px",
+  position: "relative",
+  paddingTop: theme.spacing(12),
+  paddingLeft: theme.spacing(20),
+  paddingRight: theme.spacing(20),
+  [theme.breakpoints.down("xl")]: {
+    paddingLeft: theme.spacing(10),
+    paddingRight: theme.spacing(10),
+  },
+  [theme.breakpoints.down("md")]: {
+    paddingLeft: theme.spacing(6.75),
+    paddingRight: theme.spacing(6.75),
+  },
+  [theme.breakpoints.down("xs")]: {
+    flexDirection: "column",
+    alignItems: "center",
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    height: "630px",
+  },
+}))
 
-const LinksContainer = styled(Box)(() => {
-  const theme = useTheme()
-  return {
-    display: "flex",
-    width: "710px",
-    justifyContent: "space-around",
+const LinksContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  width: "710px",
+  justifyContent: "space-around",
 
-    [theme.breakpoints.down("xs")]: {
-      paddingLeft: "24px",
-      paddingRight: "24px",
-      width: "100%",
-    },
-  }
-})
+  [theme.breakpoints.down("xs")]: {
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    width: "100%",
+  },
+}))
 
 const LinksWrapper = styled(Box)({
   display: "flex",
   flexDirection: "column",
 })
 
-const LinksTitle = styled(Typography)(() => {
-  const theme = useTheme()
-  return {
-    color: theme.palette.text.secondary,
-    textTransform: "uppercase",
-    marginBottom: "16px",
-    marginLeft: "5px",
-    fontWeight: 500,
-  }
-})
+const LinksTitle = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  textTransform: "uppercase",
+  marginBottom: theme.spacing(2),
+  marginLeft: theme.spacing(0.625),
+  fontWeight: 500,
+}))
 
-const IconButton = styled(Button)(() => {
-  const theme = useTheme()
-  return {
-    cursor: "pointer",
-    justifyContent: "flex-start",
-    marginBottom: "4px",
+const IconButton = styled(Button)(({ theme }) => ({
+  cursor: "pointer",
+  justifyContent: "flex-start",
+  marginBottom: theme.spacing(0.5),
+  "&.MuiButton-sizeMedium.MuiButton-textSecondary:not(.Mui-disabled):not(.Mui-focusVisible):not(:hover)":
+    {
+      color: theme.palette.text.primary,
+      textTransform: "capitalize",
+      "& .MuiButton-startIcon svg.MuiSvgIcon-root": {
+        color: theme.palette.text.secondary,
+      },
+    },
+  "&.MuiButton-sizeMedium.MuiButton-textSecondary:not(.Mui-disabled):not(.Mui-focusVisible):hover":
+    {
+      textTransform: "capitalize",
+    },
+  "&:hover": {
+    backgroundColor: "transparent",
+  },
+  [theme.breakpoints.down("xs")]: {
     "&.MuiButton-sizeMedium.MuiButton-textSecondary:not(.Mui-disabled):not(.Mui-focusVisible):not(:hover)":
       {
-        color: theme.palette.text.primary,
-        textTransform: "capitalize",
+        fontSize: "24px",
+        fontWeight: 700,
+        lineHeight: "32px",
         "& .MuiButton-startIcon svg.MuiSvgIcon-root": {
-          color: theme.palette.text.secondary,
-        },
-      },
-    "&.MuiButton-sizeMedium.MuiButton-textSecondary:not(.Mui-disabled):not(.Mui-focusVisible):hover":
-      {
-        textTransform: "capitalize",
-      },
-    "&:hover": {
-      backgroundColor: "transparent",
-    },
-    [theme.breakpoints.down("xs")]: {
-      "&.MuiButton-sizeMedium.MuiButton-textSecondary:not(.Mui-disabled):not(.Mui-focusVisible):not(:hover)":
-        {
           fontSize: "24px",
-          fontWeight: 700,
-          lineHeight: "32px",
-          "& .MuiButton-startIcon svg.MuiSvgIcon-root": {
-            fontSize: "24px",
-          },
         },
-    },
-  }
-})
+      },
+  },
+}))
 
-const SubscribeContainer = styled(Box)(() => {
-  const theme = useTheme()
-  return {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    justifyContent: "center",
-    width: "470px",
-    [theme.breakpoints.down("xs")]: {
-      width: "100%",
-      paddingLeft: "16px",
-      paddingRight: "16px",
-      alignItems: "center",
-    },
-  }
-})
+const SubscribeContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "center",
+  width: "470px",
+  [theme.breakpoints.down("xs")]: {
+    width: "100%",
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    alignItems: "center",
+  },
+}))
 
-const SubscribeSubtitle = styled(Typography)(() => {
-  const theme = useTheme()
-  return {
-    color: theme.palette.text.disabled,
-    [theme.breakpoints.down("xs")]: {
-      paddingLeft: "8px",
-      paddingRight: "8px",
-    },
-  }
-})
+const SubscribeSubtitle = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.disabled,
+  [theme.breakpoints.down("xs")]: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+  },
+}))
 
-const SubscribeTitle = styled(Typography)(() => {
-  const theme = useTheme()
-  return {
-    color: "#fff",
-    marginBottom: "16px",
-    fontFamily: "DecentralandHero",
-    [theme.breakpoints.down("xs")]: {
-      paddingLeft: "8px",
-      paddingRight: "8px",
-      marginBottom: "24px",
-    },
-  }
-})
+const SubscribeTitle = styled(Typography)(({ theme }) => ({
+  color: "#fff",
+  marginBottom: theme.spacing(2),
+  fontFamily: "DecentralandHero",
+  [theme.breakpoints.down("xs")]: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    marginBottom: theme.spacing(3),
+  },
+}))
 
 const SubscriptionBeehiiv = styled("iframe")({
   width: "100%",
@@ -144,19 +123,16 @@ const SubscriptionBeehiiv = styled("iframe")({
   height: "125px",
 })
 
-const SocialContainer = styled(Box)(() => {
-  const theme = useTheme()
-  return {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    marginBottom: "16px",
-    width: "100%",
-    [theme.breakpoints.down("xs")]: {
-      display: "none",
-    },
-  }
-})
+const SocialContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  marginBottom: theme.spacing(2),
+  width: "100%",
+  [theme.breakpoints.down("xs")]: {
+    display: "none",
+  },
+}))
 
 const SocialIconWrapper = styled(Box)({
   display: "flex",
