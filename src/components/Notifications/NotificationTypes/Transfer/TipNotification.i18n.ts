2fbe30d77d4b2ce33783674cfebaedd57d36a3bf
Andres Morelos <6563162+AndresMorelos@users.noreply.github.com>
2025-12-24 09:48:51 -0500

    feat: enhance Profile component with address copy functionality and display options
    - Added a new prop to show both name and address in the Profile component.
    - Implemented an address copy feature using an IconButton with a ContentCopyIcon.
    - Updated Profile stories to demonstrate the new display option for name and address.

--- src/components/Notifications/NotificationTypes/Transfer/TipNotification.i18n.ts
+++ src/components/Notifications/NotificationTypes/Transfer/TipNotification.i18n.ts
@@ -1,16 +1,16 @@
 const i18n = {
   en: {
     description: "",
-    title: "{playerName} sent you {manaAmount} tip for your {sceneName} scene!",
+    title: "{playerName} sent you {manaAmount} tip for one of your scenes!",
   },
   es: {
     description: "",
     title:
-      "¡{playerName} te envió una propina de {manaAmount} por tu escena {sceneName}!",
+      "¡{playerName} te envió una propina de {manaAmount} por una de tus escenas!",
   },
   zh: {
     description: "",
-    title: "{playerName} 为你的 {sceneName} 场景发送了 {manaAmount} 小费！",
+    title: "{playerName} 为你的其中一个场景发送了 {manaAmount} 小费！",
   },
 }
 
