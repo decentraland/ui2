0815e57aacd40b361beeedc3a72b4ce36b2eac1b
Braian Mellor <braianj@gmail.com>
2026-03-18 13:08:57 -0300

    feat: add AnimatedBackground component with WebGL shader and customizable color/pattern (#402)
    * feat: add AnimatedBackground component with WebGL shader and customizable color/pattern
    
    * fix: format
    
    * fix: lint
    
    * chore: update dependencies

---
+++ src/components/AnimatedBackground/AnimatedBackground.types.ts
@@ -0,0 +1,9 @@
+type AnimatedBackgroundVariant = 'fixed' | 'absolute'
+
+type AnimatedBackgroundProps = {
+  variant?: AnimatedBackgroundVariant
+  color?: string
+  patternUrl?: string
+}
+
+export type { AnimatedBackgroundVariant, AnimatedBackgroundProps }
